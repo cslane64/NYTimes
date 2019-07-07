82ecbaf7cae45abc345cb52f0bdbb281ebf3691e
cslane64 <cslane@gmail.com>
2019-07-07 18:10:39 -0400

    last try

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ require("./config/routes")(app);
 //var databaseUrl = "times_db";
 //var collections = ["articles"];
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user:password1@ds347917.mlab.com:47917/heroku_6kln3gk4";
-mongoose.connect("mongodb://localhost/times_db", { useNewUrlParser: true }, function(error){
+var MONGODB_URI = process.env.MONGODB_URI || mongoose.connect("mongodb://user:password1@ds347917.mlab.com:47917/heroku_6kln3gk4", { useNewUrlParser: true }, function(error){
+//mongoose.connect("mongodb://localhost/times_db", 
   // if (error) {
   //   console.log("database connection error" + error)
   // } else {
